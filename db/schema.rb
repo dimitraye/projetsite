b78353a34ec338c5c15795469baee16c5d585dde
niMo <insitapim@gmail.com>
2018-08-28 17:07:09 +0200

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180828061643) do
+ActiveRecord::Schema.define(version: 20180828130441) do
 
   create_table "authors", force: :cascade do |t|
     t.string "firstname"
